6c0bb8ab8302bc78f38e5d6c34029219b5779262
vikashiscool <vikash_parekh@yahoo.com>
2016-02-21 15:53:45 -0800

    First commit. Create app, index, style.

---
+++ app.js
@@ -0,0 +1,15 @@
+(function( $ ) {
+ 
+    $.fn.slider = function( action ) {
+ 
+        if ( action === "hover") {
+            // Open popup code.
+        }
+ 
+        if ( action === "slide" ) {
+            // Close popup code.
+        }
+ 
+    };
+ 
+}( jQuery ));
